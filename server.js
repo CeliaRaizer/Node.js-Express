947c18cfcbf2a2401bb4f9927f28d5c4dae23716
Célia Cristina Raizer <celia.cristina.slongo.raizer@gmail.com>
2025-10-12 15:09:55 -0300

    Atualiza API de livros com persistência e middleware de erros

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require("./src/app");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log(`Servidor rodando em http://localhost:${PORT}`);
+require('dotenv').config(); //carrega as variaves do arquivo .env
+
+const app = require("./src/app"); //importa o módulo que esta no arquivo /src/app
+
+const PORT = process.env.PORT || 3000; // Define a porta do servidor (usa .env ou 3000 por padrão)
+const NODE_ENV = process.env.NODE_ENV || 'development'; // Define o ambiente 
+
+app.listen(PORT, () =>{
+    console.log(`Servidor iniciado na porta ${PORT} (${NODE_ENV})`);
 });
