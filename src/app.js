954cd14313aed7c1a8eeb412f86ed9224529d6ac
Célia Cristina Raizer <celia.cristina.slongo.raizer@gmail.com>
2025-10-12 16:23:30 -0300

    Implementa paginação em GET /api/livros e atualiza rotas

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const routes = require("./routes"); //todas as rotas da aplicação (centralizad
 const errorHandler = require("./middleware/errorHandler") //configura o middleware de tratamento de erros
 
 app.use("/api", routes); //configura as rotas com prefixo /api
+
 app.use(errorHandler); //middleware de tratamento de erros
+
 app.use((req, res) => { //handler para rotas não encontradas (404)
     res.status(404).json({erro: "Endpoint não encontrado"});
 });
